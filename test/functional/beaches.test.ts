87a284f7614bef3743e07016b95f36b418e673e8
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-07-06 20:12:38 -0300

    Add error testing for validation

--- test/functional/beaches.test.ts
+++ test/functional/beaches.test.ts
@@ -1,4 +1,8 @@
+import { Beach } from '@src/models/beach';
+
 describe('Beaches functional tests', () => {
+  beforeAll(async () => await Beach.deleteMany({}));
+
   describe('When creating a beach', () => {
     it('should create a beach with success', async () => {
       const newBeach = {
@@ -12,6 +16,39 @@ describe('Beaches functional tests', () => {
 
       expect(response.status).toBe(201);
       expect(response.body).toEqual(expect.objectContaining(newBeach));
+    });
+
+    it('should return 422 when there is a validation error', async () => {
+      const newBeach = {
+        lat: -33.792726,
+        lng: 'invalid_value',
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(422);
+      expect(response.body).toEqual({
+        error:
+          'Beach validation failed: lng: Cast to Number failed for value "invalid_value" (type string) at path "lng"',
+      });
+    });
+
+    it.skip('should return 500 when there is any error other than validation error', async () => {
+      const newBeach = {
+        lat: -33.792726,
+        lng: 'invalid_value',
+        name: 'Manly',
+        position: 'E',
+      };
+
+      const response = await global.testRequest.post('/beaches').send(newBeach);
+
+      expect(response.status).toBe(500);
+      expect(response.body).toEqual({
+        error: 'Internal Server Error',
+      });
     });
   });
 });

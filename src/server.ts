fdf3f8ccda2f122e372d7dd50cdc8145bd8c65c5
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-07-06 15:33:29 -0300

    Add database initial configuration
    This will add the necessary configuration for opening and closing the
    mongodb connection and update the testing suit
    Also adds the mongoose dependency

--- src/server.ts
+++ src/server.ts
@@ -3,19 +3,17 @@ import { Server } from '@overnightjs/core';
 import bodyParser from 'body-parser';
 import { ForecastController } from './controllers/forecast';
 import { Application } from 'express';
+import * as database from '@src/database';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
     super();
   }
 
-  public init(): void {
+  public async init(): Promise<void> {
     this.setupExpress();
     this.setupControllers();
-  }
-
-  public getApp(): Application {
-    return this.app;
+    await this.databaseSetup();
   }
 
   private setupExpress(): void {
@@ -25,5 +23,17 @@ export class SetupServer extends Server {
   private setupControllers(): void {
     const forecastController = new ForecastController();
     this.addControllers([forecastController]);
+  }
+
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
+  public getApp(): Application {
+    return this.app;
   }
 }

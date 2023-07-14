9c3d5f34d448e72acc0103ff0ecd0ab4143303a6
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-07-13 21:14:23 -0300

    Add custom validator to check unique email

--- src/models/user.ts
+++ src/models/user.ts
@@ -7,6 +7,10 @@ export interface User {
   password: string;
 }
 
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
+}
+
 const schema = new mongoose.Schema<User>(
   {
     name: { type: String, required: true },
@@ -26,6 +30,15 @@ const schema = new mongoose.Schema<User>(
       },
     },
   }
+);
+
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
 );
 
 export const User: Model<User> = mongoose.model('User', schema);

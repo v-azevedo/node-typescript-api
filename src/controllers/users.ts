bcf135e1d3eee7013d9b1216262d86c68c78fc5b
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-07-13 23:08:21 -0300

    Add the route to authenticate the user with jwt

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { User } from '@src/models/user';
 import { Request, Response } from 'express';
 import { BaseController } from '.';
+import AuthService from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -15,5 +16,26 @@ export class UsersController extends BaseController {
     } catch (err) {
       this.sendCreatedUpdatedErrorResponse(res, err);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+
+    if (!user)
+      return res.status(401).send({ code: 401, error: 'User not found!' });
+
+    if (!(await AuthService.comparePassword(password, user.password)))
+      return res
+        .status(401)
+        .send({ code: 401, error: 'Password does not match!' });
+
+    const token = AuthService.generateToken(user.toJSON());
+
+    return res.status(200).send({ token: token });
   }
 }

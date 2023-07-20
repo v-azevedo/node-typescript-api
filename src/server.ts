ddacb52d781cf38e45aafcccd0a59afe2e811ab0
Victor Azevedo <victorsilvadeazevedoj@gmail.com>
2023-07-20 16:18:29 -0300

    Add add openapi and custom error middleware
    This commit will add the implementation for OpenAPI schema documentation
    
    Will also implement the swagger UI on the route /docs

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,11 @@ import * as database from '@src/database';
 import { BeachesController } from './controllers/beaches';
 import { UsersController } from './controllers/users';
 import logger from './logger';
+import apiSchema from './api.schema.json';
+import swaggerUi from 'swagger-ui-express';
+import * as OpenApiValidator from 'express-openapi-validator';
+import { OpenAPIV3 } from 'express-openapi-validator/dist/framework/types';
+import { apiErrorValidator } from './middleware/api-error-validator';
 
 export class SetupServer extends Server {
   constructor(private port = 3000) {
@@ -17,14 +22,20 @@ export class SetupServer extends Server {
 
   public async init(): Promise<void> {
     this.setupExpress();
+    this.docsSetup();
     this.setupControllers();
     await this.databaseSetup();
+    this.setupErrorHandlers();
   }
 
   private setupExpress(): void {
     this.app.use(bodyParser.json());
     this.app.use(pinoHttp({ logger }));
     this.app.use(cors({ origin: '*' }));
+  }
+
+  private setupErrorHandlers(): void {
+    this.app.use(apiErrorValidator);
   }
 
   private setupControllers(): void {
@@ -37,6 +48,18 @@ export class SetupServer extends Server {
       beachesController,
       usersController,
     ]);
+  }
+
+  private docsSetup(): void {
+    this.app.use('/docs', swaggerUi.serve, swaggerUi.setup(apiSchema));
+
+    this.app.use(
+      OpenApiValidator.middleware({
+        apiSpec: apiSchema as OpenAPIV3.Document,
+        validateRequests: true,
+        validateResponses: true,
+      })
+    );
   }
 
   private async databaseSetup(): Promise<void> {
